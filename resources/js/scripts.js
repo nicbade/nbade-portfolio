2af3738c12b9da942fda1931d9b94d0c46416f6a
Nic Bade <chipchapmaniii@gmail.com>
2019-02-21 09:35:20 -0600

    Added logo, made sticky header permenant and added appointy scheduler iframe

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     // STICKY NAV AT ABOUT US
-    $('.js--aboutUs').waypoint(function(direction) {
-        if(direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '60px'
-    })
+    // $('.js--aboutUs').waypoint(function(direction) {
+    //     if(direction == "down") {
+    //         $('nav').addClass('sticky');
+    //     } else {
+    //         $('nav').removeClass('sticky');
+    //     }
+    // }, {
+    //     offset: '60px'
+    // })
 
     // MOBILE NAVIGATION
     $('.js--nav-icon').click(function() {
